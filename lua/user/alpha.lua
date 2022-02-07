9222975041ca0ccca690d1bbbcde7464ce9584ad
ishamm <ishamm@penguin>
2022-02-07 23:41:52 +0530

    Customize starting screen, fix deprecation.

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -5,12 +5,14 @@ end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+[[  /$$$$$$  /$$$$$$$  /$$$$$$$$  /$$$$$$  /$$      /$$ ]],
+[[ /$$__  $$| $$__  $$| $$_____/ /$$__  $$| $$$    /$$$ ]],
+[[| $$  \__/| $$  \ $$| $$      | $$  \ $$| $$$$  /$$$$ ]],
+[[| $$      | $$$$$$$/| $$$$$   | $$$$$$$$| $$ $$/$$ $$ ]],
+[[| $$      | $$__  $$| $$__/   | $$__  $$| $$  $$$| $$ ]],
+[[| $$    $$| $$  \ $$| $$      | $$  | $$| $$\  $ | $$ ]],
+[[|  $$$$$$/| $$  | $$| $$$$$$$$| $$  | $$| $$ \/  | $$ ]],
+[[ \______/ |__/  |__/|________/|__/  |__/|__/     |__/ ]],
 }
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
@@ -28,7 +30,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "camelCaseAdvocate"
 end
 
 dashboard.section.footer.val = footer()
